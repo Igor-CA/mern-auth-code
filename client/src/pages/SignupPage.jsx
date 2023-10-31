9050540210dbf6b9dd98d95c535269618b8e6274
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-10-31 18:41:50 -0300

    Changes to style using tailwindCSS

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -28,13 +28,14 @@ export default function SignupPage() {
 				headers: {
 					Authorization: process.env.REACT_APP_API_KEY,
 				},
-				url: `/api/user/signup`,
+				url: `/signup`,
 			});
 			navigate("/login");
 		} catch (error) {
 			const customErrorMessage = error.response.data.message;
 			setErrors((prevErrors) => [...prevErrors, customErrorMessage]);
 
+			
 			setTimeout(() => {
 				setErrors([]);
 			}, 5000);
@@ -43,11 +44,11 @@ export default function SignupPage() {
 
 	const renderErrorsMessage = () => {
 		return (
-			<div className="errors-message">
+			<div className="bg-red-100 rounded-md border border-red-400 fixed top-2 right-1/2 translate-x-1/2">
 				<div>
 					{errors.map((erro, index) => {
 						return (
-							<p key={index} className="errors-message__error">
+							<p key={index} className="text-red-400 px-10 py-1 font-semibold">
 								{erro}
 							</p>
 						);
@@ -105,16 +106,16 @@ export default function SignupPage() {
 	};
 
 	return (
-		<div className="">
-			<h1 className="">Criar conta</h1>
+		<div className="mx-auto mb-2 bg-white rounded-lg w-full max-w-sm text-center text-slate-500 shadow-2xl p-10">
+			<h1 className="text-2xl font-bold pb-12">Criar conta</h1>
 			<form
 				method="post"
-				className="autentication-form"
+				className="space-y-4 flex flex-col"
 				onSubmit={(e) => {
 					handleSubmit(e);
 				}}
 			>
-				<label htmlFor="email" className="autentication-form__label">
+				<label htmlFor="email" className="hidden">
 					Email:
 				</label>
 				<input
@@ -122,7 +123,7 @@ export default function SignupPage() {
 					name="email"
 					placeholder="Email"
 					id="email"
-					className="autentication-form__input"
+					className="bg-gray-100 p-2.5 rounded"
 					onChange={(e) => {
 						handleChange(e);
 					}}
@@ -131,7 +132,7 @@ export default function SignupPage() {
 					}}
 					required
 				/>
-				<label htmlFor="username" className="autentication-form__label">
+				<label htmlFor="username" className="hidden">
 					User name:{" "}
 				</label>
 				<input
@@ -139,7 +140,7 @@ export default function SignupPage() {
 					name="username"
 					placeholder="Nome de usuário"
 					id="username"
-					className="autentication-form__input"
+					className="bg-gray-100 p-2 rounded"
 					onChange={(e) => {
 						handleChange(e);
 					}}
@@ -150,7 +151,7 @@ export default function SignupPage() {
 					pattern="^[A-Za-z0-9]{3,16}$"
 					maxLength="16"
 				/>
-				<label htmlFor="password" className="autentication-form__label">
+				<label htmlFor="password" className="hidden">
 					Password:
 				</label>
 				<input
@@ -158,7 +159,7 @@ export default function SignupPage() {
 					name="password"
 					placeholder="Senha"
 					id="password"
-					className="autentication-form__input"
+					className="bg-gray-100 p-2 rounded"
 					onChange={(e) => {
 						handleChange(e);
 					}}
@@ -169,7 +170,7 @@ export default function SignupPage() {
 					pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"
 					minLength="8"
 				/>
-				<label htmlFor="confirm-password" className="autentication-form__label">
+				<label htmlFor="confirm-password" className="hidden">
 					Confirm password:
 				</label>
 				<input
@@ -177,7 +178,7 @@ export default function SignupPage() {
 					name="confirm-password"
 					placeholder="Confirmar senha"
 					id="confirm-password"
-					className="autentication-form__input"
+					className="bg-gray-100 p-2 rounded"
 					onChange={(e) => {
 						handleChange(e);
 					}}
@@ -187,12 +188,12 @@ export default function SignupPage() {
 					required
 					pattern={formData.password}
 				/>
-				<label htmlFor="tos-checkbox" className="form__label--visible">
+				<label htmlFor="tos-checkbox" className="text-justify">
 					<input
 						type="checkbox"
 						name="tos-checkbox"
 						id="tos-checkbox"
-						className=""
+						className="mr-2"
 						required
 						onInvalid={(e) => {
 							handleInvalid(e);
@@ -201,15 +202,12 @@ export default function SignupPage() {
 							handleChange(e);
 						}}
 					/>
-					Ao marcar esta aba você concorda com nossos{" "}
-					<Link to={"/tos"} className="autentication-form__link">
-						termos e condições
-					</Link>
+					Ao marcar esta aba você concorda com nosso <strong className="font-semibold text-blue-400 cursor-pointer">termos e condições</strong>
 				</label>
-				<button className="button" style={{ margin: "10px" }}>
+				<button className="bg-blue-400 text-white p-1 w-full font-semibold rounded-md">
 					Criar conta
 				</button>
-				<Link to={"/login"} className="autentication-form__link">
+				<Link to={"/login"} className="text-blue-400">
 					Já tem conta?
 				</Link>
 			</form>
