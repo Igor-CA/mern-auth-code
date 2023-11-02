18095733b090e09874b22c10ba29c9a9c1401f8f
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-11-02 13:21:00 -0300

    Create simple main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
-import TestPage from "./pages/TestPage";
+import MainPage from "./pages/MainPage";
 import ResetPassword from "./pages/ResetPassword";
-import SendEmail from "./pages/SendEmail";
+import SendEmail from "./pages/ForgotPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 function App() {
 	
 	return (
-		<div className="container">
+		<div className="bg-gray-100">
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<TestPage />}></Route>
+					<Route path="/" element={<MainPage />}></Route>
 					<Route path="/signup" element={<SignupPage />}></Route>
 					<Route path="/login" element={<LoginPage />}></Route>
 					<Route path="/forgot" element={<SendEmail />}></Route>
